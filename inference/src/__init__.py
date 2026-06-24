6e4e9a474362890803f97bc1dff842554fc94660
Irina Epure <irinamonaepure@gmail.com>
2026-06-24 16:21:56 +0100

    Organized correlation

---
+++ inference/src/__init__.py
@@ -0,0 +1,16 @@
+from .correlation import (
+    retrieve_group,
+    generate_correlation_matrix,
+    threshold_signed_adjacency,
+    plot_correlation_network,
+    plot_correlation_matrix,
+    save_correlation_matrix,
+)
+__all__ = [
+    "retrieve_group",
+    "generate_correlation_matrix",
+    "threshold_signed_adjacency",
+    "plot_correlation_matrix",
+    "plot_correlation_network",
+    "save_correlation_matrix"
+]
