59f91945b25e549c65ff8b0f5e96fa7b5df6c929
Irina Epure <irinamonaepure@gmail.com>
2026-06-25 23:11:17 +0100

    Aggregate plots

--- inference/src/__init__.py
+++ inference/src/__init__.py
@@ -14,7 +14,12 @@ from .aggregate_plots import (
     load_weight_matrix,
     get_off_diagonal_weights,
     plot_all_correlation_distributions,
-    plot_first_column_sums,
+    plot_first_column_averages,
+    plot_total_correlation_summaries,
+    save_top_beliefs_by_absolute_edge_sum,
+    plot_top_beliefs_stacked_by_country,
+    plot_near_zero_correlation_counts,
+    plot_first_column_average_with_dendrogram
 )
 
 __all__ = [
@@ -30,5 +35,10 @@ __all__ = [
     "load_weight_matrix",
     "get_off_diagonal_weights",
     "plot_all_correlation_distributions",
-    "plot_first_column_sums",
+    "plot_first_column_averages",
+    "plot_total_correlation_summaries",
+    "save_top_beliefs_by_absolute_edge_sum",
+    "plot_top_beliefs_stacked_by_country",
+    "plot_near_zero_correlation_counts",
+    "plot_first_column_average_with_dendrogram"
 ]
